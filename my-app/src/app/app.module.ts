4562c838f179d50e01b67d6e9f5a8cebf7894890
Tsarev_Dmitry <tzariov.dmitry.work@yandex.ru>
2019-03-01 10:01:22 +0300

    few components created, html sign-in page

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,14 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule,
     AppRoutingModule
   ],
   providers: [],
