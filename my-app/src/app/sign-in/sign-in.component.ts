633905fd5ff86a2b19f448df986014b0d84d1c5b
Dmitry <tzariov.dmitry@yandex.ru>
2019-03-06 00:32:49 +0300

    login user (localstorage)

--- my-app/src/app/sign-in/sign-in.component.ts
+++ my-app/src/app/sign-in/sign-in.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
@@ -7,10 +7,33 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./sign-in.component.less']
 })
 export class SignInComponent implements OnInit {
-
+  signInForm: FormGroup;
+  private _userLogin;
   constructor() { }
 
   ngOnInit() {
+    this.signInForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required)
+    });
+  }
+
+  login() {
+    this._userLogin = {
+      email: this.signInForm.value.email,
+      password: this.signInForm.value.password
+    };
+    let users = JSON.parse(localStorage.getItem('users'));
+    console.log("all users", users);
+    let userAccepted = users
+      .filter(item => item.email === this._userLogin.email)
+      .filter(item => item.password === this._userLogin.password);
+    console.log("accepted users", userAccepted);
+    if (userAccepted && userAccepted.length === 1) {
+      localStorage.setItem('currentUser', JSON.stringify(userAccepted[0]));
+    }
+
+    console.log("signINFORM!");
   }
 
 }
