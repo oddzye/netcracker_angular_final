e5210c56db24f9d1d336facf03123aa886b20c95
Dmitry <tzariov.dmitry@yandex.ru>
2019-03-09 23:43:10 +0300

    mongo schema

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,16 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const morgan = require('morgan');
 const authRoutes = require('./routes/auth');
 const bodyParser = require('body-parser');
+const app = express();
+
+mongoose.connect('')
+    .then(() => console.log("Mongo started"))
+    .catch((err) => console.log(err));
+app.use(morgan('dev'));
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use('/auth', authRoutes);
