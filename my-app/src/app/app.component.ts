2ee80eefaf35add6b6b38b78bf097063ccae784d
Tsarev_Dmitry <tzariov.dmitry.work@yandex.ru>
2019-03-12 12:11:36 +0300

    -

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less'],
+  providers: [AuthService]
 })
 export class AppComponent {
   title = 'my-app';
