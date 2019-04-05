5b14727ff40b67cce30872e83a44200172df88ca
Dmitry <tzariov.dmitry@yandex.ru>
2019-04-05 17:21:36 +0300

    -

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use('/api/auth', authRoutes);
-app.use('/article', articleRoutes);
+app.use('/api/article', articleRoutes);
 
 
 
--- backend/controllers/article.js
+++ backend/controllers/article.js
@@ -1,7 +1,8 @@
-
-
+const mongoConnection = require('../middleware/mongoConnection');
+const errorHandler = require('../utils/errorHandler');
+const Article = require('../models/Article');
 module.exports.getAll = (req, res) => {
-    res.status(200).json({message: 'Articles'})
+   
 }
 
 module.exports.getById = (req, res) => {
@@ -13,7 +14,22 @@ module.exports.remove = (req, res) => {
 }
 
 module.exports.create = (req, res) => {
-    
+    mongoConnection(async (db) => {
+        try {
+            const article = await db.collection("articles").insert(new Article({
+                title: req.body.title,
+                text: req.body.text,
+                user: req.body.user
+            }), (err, result) => {
+                if (err) console.log(`Creating acticle error, ${err}`);
+                console.log("Article created with result", result);
+                res.status(201).json(article);
+            })
+        }
+        catch (e) {
+            errorHandler(res, e);
+        }
+    })
 }
 
 module.exports.update = (req, res) => {
